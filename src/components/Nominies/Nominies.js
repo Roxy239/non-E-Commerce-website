8ca029f5d43bfa0094ee3acdb1751e73ec0df041
Roxy239 <rahatara239@gmail.com>
2021-09-26 21:33:32 +0600

    grid

--- src/components/Nominies/Nominies.js
+++ src/components/Nominies/Nominies.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './Nominies.css'
 
 const Nominies = () => {
     return (
-        <div>
-            <h2>this is Nominies</h2>
+        <div className='nominies-container'>
+            <div className="writer-container">
+                <h3>writers list</h3>
+            </div>
+            <div className="cart-container">
+                <h3>Selected top writers</h3>
+                <h5>player added</h5>
+                <h5>writers total salary: $</h5>
+            </div>
         </div>
     );
 };
